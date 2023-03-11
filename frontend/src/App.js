c8e179b91eada7316a718b9710cc594d9885d5cb
rizkisahat996 <rizkisitumeang77@gmail.com>
2023-03-11 23:13:33 +0700

    212

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,7 +14,7 @@ function App() {
   
   return (
     <div className="App">
-      <BrowserRouter basename="/react">
+      <BrowserRouter basename="/">
         <Navbar />
         <div className="pages min-h-screen relative">
           <Routes>
